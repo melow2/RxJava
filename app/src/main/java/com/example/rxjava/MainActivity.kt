e6ded479098b6d33efb1913dfb4948219068f503
amy <melow2@jeit.co.kr>
2021-01-04 12:02:02 +0900

    backpressure

--- app/src/main/java/com/example/rxjava/MainActivity.kt
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.rxjava
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.annotations.NonNull
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,22 +22,22 @@ class MainActivity : AppCompatActivity() {
     private var greetingObserver: Observer<String> = object:Observer<String>{
         override fun onSubscribe(d: Disposable?) {
             // observer를 listen하면 호출.
-            Log.d("DEBUG","onSubscribe()")
+            Log.d("DEBUG", "onSubscribe()")
         }
 
         override fun onNext(t: String?) {
             // 값을 전달할 떄 호출하여 값을 넘겨줌.
-            Log.d("DEBUG","onNext()")
+            Log.d("DEBUG", "onNext()")
             textView?.text = t
         }
 
         override fun onError(e: Throwable?) {
-            Log.d("DEBUG","onError()")
+            Log.d("DEBUG", "onError()")
         }
 
         override fun onComplete() {
             // 가지고 있는 값을 모두 전달하면 호출함.
-            Log.d("DEBUG","onComplete()")
+            Log.d("DEBUG", "onComplete()")
         }
     }
 
@@ -45,8 +45,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.tvGreeting)
-        observableGreeting = Observable.just("방출!!")
-        observableGreeting.subscribeOn(Schedulers.io())
-        observableGreeting.observeOn(AndroidSchedulers.mainThread())
+        val tmpStr: String = Arrays.stream(arrayOfNulls<String>(10000000)).map { x -> "*" }.collect(Collectors.joining())
+        val foo: Flowable<*> = Flowable.range(0, 1000000000)
+                .map { x: Int ->
+                    println("[very fast sender] i'm fast. very fast.")
+                    println(String.format("sending id: %s %d%50.50s", Thread.currentThread().name, x, tmpStr))
+                    x.toString() + tmpStr
+                }
+
+        foo.observeOn(Schedulers.computation()).subscribe { x: Any? ->
+            Thread.sleep(1000)
+            println("[very busy receiver] i'm busy. very busy.")
+            println(String.format("receiving id: %s %50.50s", Thread.currentThread().name, x))
+        }
+
+        while (true) {
+            Thread.sleep(1000)
+        }
     }
 }
