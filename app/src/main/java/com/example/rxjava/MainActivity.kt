a7911c622b623c9c97abc834011dd6b784b423b8
amy <melow2@jeit.co.kr>
2021-01-04 12:16:20 +0900

    just

--- app/src/main/java/com/example/rxjava/MainActivity.kt
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.observers.DisposableObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.*
 
@@ -15,29 +18,21 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     private val greeting = "Hello From RxJava."
-    private lateinit var observableGreeting: Observable<String>
-
+    private lateinit var myObservable: Observable<String>
+    private var compositeDisposable:CompositeDisposable = CompositeDisposable()
     private var textView: TextView?=null
 
-    private var greetingObserver: Observer<String> = object:Observer<String>{
-        override fun onSubscribe(d: Disposable?) {
-            // observer를 listen하면 호출.
-            Log.d("DEBUG", "onSubscribe()")
-        }
-
+    private var greetingObserver: DisposableObserver<String> = object: DisposableObserver<String>() {
         override fun onNext(t: String?) {
-            // 값을 전달할 떄 호출하여 값을 넘겨줌.
-            Log.d("DEBUG", "onNext()")
-            textView?.text = t
+            TODO("Not yet implemented")
         }
 
         override fun onError(e: Throwable?) {
-            Log.d("DEBUG", "onError()")
+            TODO("Not yet implemented")
         }
 
         override fun onComplete() {
-            // 가지고 있는 값을 모두 전달하면 호출함.
-            Log.d("DEBUG", "onComplete()")
+            TODO("Not yet implemented")
         }
     }
 
@@ -45,22 +40,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.tvGreeting)
-        val tmpStr: String = Arrays.stream(arrayOfNulls<String>(10000000)).map { x -> "*" }.collect(Collectors.joining())
-        val foo: Flowable<*> = Flowable.range(0, 1000000000)
-                .map { x: Int ->
-                    println("[very fast sender] i'm fast. very fast.")
-                    println(String.format("sending id: %s %d%50.50s", Thread.currentThread().name, x, tmpStr))
-                    x.toString() + tmpStr
-                }
-
-        foo.observeOn(Schedulers.computation()).subscribe { x: Any? ->
-            Thread.sleep(1000)
-            println("[very busy receiver] i'm busy. very busy.")
-            println(String.format("receiving id: %s %50.50s", Thread.currentThread().name, x))
-        }
-
-        while (true) {
-            Thread.sleep(1000)
-        }
+        myObservable = Observable.just(greeting)
+        compositeDisposable.add(
+                myObservable.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(greetingObserver)
+        )
     }
 }
