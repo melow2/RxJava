32c62f7e42f8e903db9413baeebb7294f76fc1c0
amy <melow2@jeit.co.kr>
2021-01-06 14:46:53 +0900

    readme

--- app/src/main/java/com/example/rxjava/MainActivity.kt
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -5,26 +5,26 @@ import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
+import io.reactivex.rxjava3.core.ObservableOnSubscribe
 import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.observers.DisposableObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private val greeting = "Hello From RxJava."
     private lateinit var myObservable: Observable<String>
-    private var compositeDisposable:CompositeDisposable = CompositeDisposable()
-    private var textView: TextView?=null
+    private var textView: TextView? = null
+    private val TAG = MainActivity::class.simpleName
 
-    private var greetingObserver: DisposableObserver<String> = object: DisposableObserver<String>() {
-        override fun onNext(t: String?) {
-            TODO("Not yet implemented")
+    private var tempObserver: DisposableObserver<Int> = object : DisposableObserver<Int>() {
+
+        override fun onNext(t: Int?) {
+            Log.v(TAG, "onNext() - "+t+" "+Thread.currentThread().name);
         }
 
         override fun onError(e: Throwable?) {
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onComplete() {
-            TODO("Not yet implemented")
+            Log.v(TAG, "onComplete() "+Thread.currentThread().name);
         }
     }
 
@@ -41,10 +41,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.tvGreeting)
         myObservable = Observable.just(greeting)
-        compositeDisposable.add(
-                myObservable.subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(greetingObserver)
-        )
+
+
+        val observable = Observable.create(object : ObservableOnSubscribe<Int> {
+            override fun subscribe(emitter: ObservableEmitter<Int>?) {
+                // 해당 작업은 subscribeOn(AndroidSchedulers.mainThread()) 로 인하여 main 쓰레드로 동작함
+                for (i in arrayOf(1, 2, 3, 4, 5)) {
+                    Log.v(TAG, "[MAIN_THREAD] "+Thread.currentThread().name + " : onNext " + i)
+                    emitter?.onNext(i)
+                    if (i == 5) {
+                        emitter?.onComplete()
+                    }
+                }
+            }
+        })
+
+        observable
+            .subscribeOn(AndroidSchedulers.mainThread()) // emit은 mainThread에서 진행 함.
+            .observeOn(Schedulers.io()) // 아래 작업은 observeOn(Schedulers.io()) 로인해 RxCachedThreadScheduler 쓰레드로 동작함
+            .doOnNext { i: Int ->
+                Log.v(
+                    TAG,
+                    "[IO_THREAD] "+Thread.currentThread().name + " : onNext " + i
+                )
+            }
+            .observeOn(Schedulers.computation()) // 아래 작업은 observeOn(Schedulers.computation()) 로인해 RxComputationThreadPool 쓰레드로 동작함
+            .doOnNext { i: Int ->
+                Log.v(
+                    TAG,
+                    "[COMPUTATION_THREAD] "+Thread.currentThread().name + " : onNext " + i
+                )
+            }
+            .observeOn(Schedulers.newThread()) // 아래 작업은 observeOn(Schedulers.newThread()) 로인해 RxNewThreadScheduler 쓰레드로 동작함
+            .subscribe(tempObserver)
+
     }
 }
